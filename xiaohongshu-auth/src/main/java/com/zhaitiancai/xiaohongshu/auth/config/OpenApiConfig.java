d2c63b918a51d4359e8b709edf5a4bb5eb4e776d
zhai <2606757760@qq.com>
2025-09-16 14:08:54 +0800

    整合redistemplate 测试登录 [写了一个初始线程池暂未应用]

--- xiaohongshu-auth/src/main/java/com/zhaitiancai/xiaohongshu/auth/config/OpenApiConfig.java
+++ xiaohongshu-auth/src/main/java/com/zhaitiancai/xiaohongshu/auth/config/OpenApiConfig.java
@@ -10,30 +10,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-@Bean
-public OpenAPI customOpenAPI() {
-    return new OpenAPI()
-            .info(new Info()
-                    .title("小红书认证系统 API")
-                    .description("提供认证相关的 RESTful API")
-                    .version("1.0.0")  // 添加版本号
-                    .termsOfService("http://example.com/terms")
-                    .license(new License()
-                            .name("Apache 2.0")
-                            .url("http://www.apache.org/licenses/LICENSE-2.0.html")
-                    )
-            )
-            .externalDocs(new ExternalDocumentation()
-                    .description("项目文档")
-                    .url("https://github.com/your-repo/docs")
-            );
-}
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("小红书认证系统 API")
+                        .description("提供认证相关的 RESTful API")
+                        .version("1.0.0")  // 添加版本号
+                        .termsOfService("http://example.com/terms")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")
+                        )
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("项目文档")
+                        .url("https://github.com/your-repo/docs")
+                );
+    }
 
-@Bean
-public GroupedOpenApi publicApi() {
-    return GroupedOpenApi.builder()
-            .group("Web 后台接口")
-            .pathsToMatch("/test/**")
-            .build();
-}
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Web 后台接口")
+                .pathsToMatch("/test/**","/verification/**")
+                .build();
+    }
 }
